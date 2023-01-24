b15c6077c289408cb80a891e9e4bff35b2b6d02b
MWasile <maaciej.wasilewski@gmail.com>
2023-01-24 15:48:58 +0100

    install django-cors-headers, setup for work with react app (to change later!!!)

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -43,8 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'corsheaders',
     'rest_framework',
-
 
     'users.apps.UsersConfig',
     'dashboard.apps.DashboardConfig',
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -179,3 +180,9 @@ SIMPLE_JWT = {
     'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
+
+# CORS settings
+
+CORS_ALLOWED_ORIGINS = [
+   "http://localhost:1234",
+]
